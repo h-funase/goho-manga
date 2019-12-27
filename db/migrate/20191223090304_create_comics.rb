1efcc73ae13b2597efe424ba5d2dd4cfd112c25e
h-funase <h.funse@gmail.com>
2019-12-27 15:31:30 +0900

    編集機能がおかしい
    episodeテーブルの初期値が入らない
    タグなどの更新が効いてない

--- db/migrate/20191223090304_create_comics.rb
+++ db/migrate/20191223090304_create_comics.rb
@@ -3,15 +3,9 @@ class CreateComics < ActiveRecord::Migration[5.2]
     create_table :comics do |t|
       t.string :title,        null: false, index: true
      
-      t.text :url,             null: false
-     
-      t.integer :episode,      null: false, default: 1
-
-      t.integer :like_count,   unique: true
-
       t.text :image,           null: false
      
-      t.text :text,            null: false
+      t.text :text
 
       t.timestamps
     end
--- db/migrate/20191226094139_create_episodes.rb
+++ db/migrate/20191226094139_create_episodes.rb
@@ -2,7 +2,7 @@ class CreateEpisodes < ActiveRecord::Migration[5.2]
   def change
     create_table :episodes do |t|
       t.integer :story_number
-      t.string :url ,default: 1
+      t.string :url , null: false
       t.timestamps
     end
   end
